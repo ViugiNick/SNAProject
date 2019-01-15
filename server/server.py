baf36fcc5031fcaea52f1e67045a92b9a996367f
Nickolay.Viuginov <nickolay.viuginov@jetbrains.com>
2019-01-15 10:01:23 +0300

    server sentiment implementation

--- server/server.py
+++ server/server.py
@@ -1,8 +1,26 @@
 from flask import Flask
 from flask import request, make_response, Response, jsonify
+from keras.models import model_from_json
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+import pickle
+import pandas as pd
+import numpy as np
+import sys
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+with open('models/sent_tokenizer.pickle', 'rb') as handle:
+    sent_tokenizer = pickle.load(handle)
+
+max_len = 130
+
+json_file = open('models/SentModel.json', 'r')
+loaded_sent_model = model_from_json(json_file.read())
+json_file.close()
+loaded_sent_model.load_weights("models/SentModel.h5")
+loaded_sent_model.compile(optimizer='Adagrad', loss='categorical_crossentropy', metrics=['acc'])
 
 @app.route('/get_tweet_sentiment', methods=['POST', 'GET'])
 def pos_neg_request():
@@ -10,12 +28,15 @@ def pos_neg_request():
         try:
             body = request.json
             tweet_id = body["tweetId"]
-            tweet_message = body["tweetMessage"]
+            tweet_message = BeautifulSoup(body["tweetMessage"], "lxml").text
+#            tweet_message = ''.join([s for s in tweet_message if s in
+#                          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz '])
 
-            # TODO
-            # count sentiment
+            seq = sent_tokenizer.texts_to_sequences(tweet_message)
+            padded = pad_sequences(seq, maxlen=max_len)
+            pred = loaded_sent_model.predict(padded)
 
-            return jsonify(neg=1, pos=0)
+            return jsonify(neg=pred[2], pos=pred[3])
         except KeyError as err:
             app.logger.warning("Bad post data")
     else:
--- src/content.js
+++ src/content.js
@@ -1,33 +1,27 @@
 const SERVER = 'http://localhost:5000';
 
 Array.from(document.getElementsByClassName('tweet')).forEach(messageElement => {
-    tweet_id = messageElement.getAttribute('data-tweet-id');
+    let tweet_id = messageElement.getAttribute('data-tweet-id');
 
-    content_element = messageElement.getElementsByClassName("tweet-text")[0];
+    let content_element = messageElement.getElementsByClassName("tweet-text")[0];
     sentimentRequest(content_element.innerHTML, messageElement, tweet_id, addSentiment);
 });
 
 function addSentiment(parent, value) {
-    value = value.neg;
-    console.log('addSentiment ');
+    value = value.pos;
     const idElement = document.createElement('div');
     idElement.className = 'tweet-sentiment';
     idElement.style.backgroundColor = getColorForPercentage(value);
     idElement.title = 'Sentiment: ' + (value * 100).toFixed(2) + '%';
     parent.parentNode.insertBefore(idElement, parent);
-    //visualisedCache.set(id, idElement);
 }
 
 function sentimentRequest(tweet_message, tweet_element, tweet_id, callback) {
-  console.log('sentimentRequest ' + tweet_id);
-
   const urlLoad = SERVER + `/get_tweet_sentiment`;
   const data = JSON.stringify({
     tweetId: tweet_id,
       tweetMessage: tweet_message
   });
-
-  console.log(data);
 
   const request = new XMLHttpRequest();
   request.open('POST', urlLoad, true);
@@ -43,6 +37,6 @@ function sentimentRequest(tweet_message, tweet_element, tweet_id, callback) {
   request.onerror = function (error) {
     console.error(error);
     callback(tweet_element, null);
-  }
+  };
   request.send(data);
 }
